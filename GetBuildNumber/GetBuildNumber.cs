150aec24cf983845360e268aa96ae7fd9348bdd8
Frode Nilsen <frode@postkassa.no>
2025-02-23 18:33:45 +0100

    feat: add generation of build numbers and MSBuild task for setting FileVersion

---
+++ GetBuildNumber/GetBuildNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Kodefabrikken.GetBuildNumber
+{
+    public class GetBuildNumber : Task
+    {
+        static readonly DateTime _January1th2025 = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
+        /// A build number constructed as days since January 1th 2025 (UTC).
+        /// </summary>
+        /// <remarks>
+        /// <see cref="System.Diagnostics.FileVersionInfo"/> calls this property <see cref="System.Diagnostics.FileVersionInfo.FileBuildPart"/> with 16 bit length.
+        /// </remarks>
+        [Output]
+        public int Build { get; set; }
+
+        /// <summary>
+        /// A revision number constructed as seconds / 2 since midnight (UTC).
+        /// </summary>
+        /// <remarks><see cref="System.Version"/> splits this in <see cref="System.Version.MajorRevision"/> and <see cref="System.Version.MinorRevision"/> each 16 bit. 
+        /// <see cref="System.Diagnostics.FileVersionInfo"/> calls this property <see cref="System.Diagnostics.FileVersionInfo.FilePrivatePart"/> with 16 bit length.
+        /// </remarks>
+        [Output]
+        public int Revision { get; set; }
+
+        /// <summary>
+        /// Constructs the build number parts.
+        /// </summary>
+        /// <returns>Always true.</returns>
+        public override bool Execute()
+        {
+            var now = DateTime.UtcNow;
+            Build = (now - _January1th2025).Days;
+            Revision = (int) now.TimeOfDay.TotalSeconds / 2;
+
+            return true;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+﻿# Get build number
+
+This is a simple tool for setting the build number, the last two numbers, in assembly property FileVersion when running MSBuild.
+Whatever first two numbers are predefined in **FileVersion** is kept. These normally corresponds to major and minor version of the built file.
+
+## How it works
+If environment variable BUILD_BUILDNUMBER is set, e.g. in an Azure pipeline, then that value is used.
+Otherwise a number is created from properties **BuildNumber** and **BuildRevision** - which is also created by this tool.
+
+This override of **FileVersion** is not performed when building inside Visual Studio unless the property **SetBuildNumber** is set in the project file.
+The **FileVersion** also has to be on the form x.y.0 or x.y.0.0, else the predefined numbers are kept.
+
+**BuildNumber** is the number of days since 1th of January 2025, and **BuildRevision** is the number of seconds since midnight divided by two.
+Kind of the same logic used by Visual Studio when using the x.y.* pattern in project settings and setting Deterministic to false.
+
+The values are generated before target BeforeBuild, and in UTC time.
+
+## Why
+When building a project with a lot of files it is helpfull to keep track of the individual files that has been changed between each build.
+**FileVersion** is then use for that, while AssemblyVersion is used more for keeping track of breaking vs non-breaking changes and releases.
+If coupled with build tools like [GitVersion](https://gitversion.net) you get a complete automated system.
